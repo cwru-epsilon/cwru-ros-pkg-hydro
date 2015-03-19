f86159ae7bb98237757443a946f1a16137f9cc8d
aab119-new <aab119-new@Atlas4-optiplex.(none)>
2015-03-19 13:30:28 -0400

    Alaa - Added one line in the path_sender to identify "map"

--- catkin/src/cwru_376_student/ps5_epsilon/epsilon_des_state_generator/src/epsilon_path_sender.cpp
+++ catkin/src/cwru_376_student/ps5_epsilon/epsilon_des_state_generator/src/epsilon_path_sender.cpp
@@ -4,6 +4,7 @@
 // transmits a hard-coded path to desired-state generator node via service "appendPathService"
 // the message must contain a nav_msgs/Path object
 
+
 #include <stdlib.h>
 #include <string>
 #include <vector>
@@ -57,6 +58,7 @@ int main(int argc, char **argv) {
     double x,y,phi;
 
     vertex.header.stamp = ros::Time::now(); // look up the time and put it in the header; use same time stamp for all vertices in this path
+    vertex.header.frame_id = "map"; // specify this, so tf will know how to transform it
     
     // fill in the interesting data: (x,y) and phi = location and heading
     //vertex 1:
