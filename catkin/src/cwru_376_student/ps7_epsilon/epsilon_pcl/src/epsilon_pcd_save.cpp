8f0c0731fc1a7bb8ffd4acd11b2ca6d59d70b739
abby <abby@abby.(none)>
2015-04-17 12:15:09 -0400

    Alaa - A try to fix transform with kinect

--- catkin/src/cwru_376_student/ps7_epsilon/epsilon_pcl/src/epsilon_pcd_save.cpp
+++ catkin/src/cwru_376_student/ps7_epsilon/epsilon_pcl/src/epsilon_pcd_save.cpp
@@ -29,6 +29,9 @@
 
 //PointCloud::Ptr pclCloud(new PointCloud); // Holds the whole pcl cloud
 //typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+#include <tf/transform_listener.h> //for transforms
+
 using namespace std;
 using namespace Eigen;
 using namespace pcl;
@@ -39,18 +42,53 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr g_pclKinect(new PointCloud<pcl::PointXYZ>);
 bool got_cloud = false;
 bool saved_file = false;
 
+const tf::TransformListener* tfListener_;
+tf::StampedTransform kToB_;    
+
 void kinectCB(const sensor_msgs::PointCloud2ConstPtr& cloud) {
     pcl::PointCloud<pcl::PointXYZ>::Ptr pclKinect(new PointCloud<pcl::PointXYZ>);
+    //const sensor_msgs::PointCloud2ConstPtr temp = kinect_to_base_link(*cloud);
     pcl::fromROSMsg(*cloud, *g_pclKinect);
     //ROS_INFO("kinectCB %d * %d points", pclKinect->width, pclKinect->height);
         got_cloud=true; //cue to "main" to save snapshot
 }
+
+//sensor_msgs::PointCloud2 kinect_to_base_link(const sensor_msgs::PointCloud2ConstPtr& cloud) {
+    // to use tf, need to convert coords from a geometry_msgs::Pose into a tf::Point
+
+  // sensor_msgs::PointCloud2 temp_pcl; 
+    
+    //tfListener_->lookupTransform("base_link", "kinect_pc_frame", ros::Time(0), kToB_);
+    //let's transform the map_pose goal point into the odom frame:
+    //cloud->header.stamp = ros::Time::now();
+    //tfListener_->transformPointCloud ("base_link", cloud, temp_pcl);
+
+    //return temp_pcl; // 
+//}
 
 int main(int argc, char** argv) {
     // Do some initialization here
     ros::init(argc, argv, "process_pcl");
     ros::NodeHandle nh;
     ros::Rate rate(2);
+
+    bool tferr=true;
+    ROS_INFO("waiting for tf between kinect_pc_fram and base_link...");
+//    while (tferr) {
+//        tferr=false;
+//        try {
+                //try to lookup transform from target frame "base_link" to source frame "kinect_pc_fram"
+            //The direction of the transform returned will be from the target_frame to the source_frame. 
+             //Which if applied to data, will transform data in the source_frame into the target_frame. See tf/CoordinateFrameConventions#Transform_Direction
+//                tfListener_->lookupTransform("base_link", "kinect_pc_frame", ros::Time(0), kToB_);
+//            } catch(tf::TransformException &exception) {
+//                ROS_ERROR("%s", exception.what());
+//                tferr=true;
+//                ros::Duration(0.5).sleep(); // sleep for half a second
+//                ros::spinOnce();                
+//            }   
+//    }
+//    ROS_INFO("tf is good");
     // Subscribers
     ros::Subscriber getPCLPoints = nh.subscribe<sensor_msgs::PointCloud2> ("/kinect/depth/points", 1, kinectCB);
 
