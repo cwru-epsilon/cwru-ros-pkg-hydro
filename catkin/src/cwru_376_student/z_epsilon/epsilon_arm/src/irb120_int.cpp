5542f0f0728d8dcc4733b9b9cb71b45fb7c70ed6
albado <albado@albado-pc.(none)>
2015-04-22 18:23:10 -0400

    Alaa&Josh - Home and Trans positioning for arm motion

--- catkin/src/cwru_376_student/z_epsilon/epsilon_arm/src/irb120_int.cpp
+++ catkin/src/cwru_376_student/z_epsilon/epsilon_arm/src/irb120_int.cpp
@@ -24,7 +24,7 @@
 //callback to subscribe to marker state
 Eigen::Vector3d g_p;
 Vectorq6x1 g_q_state;
-Vectorq6x1 home_state; //= {0.0245981, -1.43767, 0.0514272, -0.13549, -0.363732, -0.0716817};
+Vectorq6x1 home_state, trans_state; //= {0.0245981, -1.43767, 0.0514272, -0.13549, -0.363732, -0.0716817};
 
 double g_x,g_y,g_z;
 //geometry_msgs::Quaternion g_quat; // global var for quaternion
@@ -168,11 +168,13 @@ void stuff_trajectory( Vectorq6x1 qvec, trajectory_msgs::JointTrajectory &new_tr
 
 
 //command robot to move to "qvec" using a trajectory message, sent via ROS-I
-void stuff_trajectory_epsilon( std::vector<Vectorq6x1> all_qvec, trajectory_msgs::JointTrajectory &new_trajectory, int nsolns) {
+void stuff_trajectory_epsilon( Vectorq6x1 qvec, trajectory_msgs::JointTrajectory &new_trajectory, int nsolns) {
     
-    trajectory_msgs::JointTrajectoryPoint trajectory_point1;
-    trajectory_msgs::JointTrajectoryPoint trajectory_point2; 
-    Vectorq6x1 qvec; 
+    trajectory_msgs::JointTrajectoryPoint trajectory_point_h;
+    trajectory_msgs::JointTrajectoryPoint trajectory_point_g;
+    trajectory_msgs::JointTrajectoryPoint trajectory_point_t;
+    trajectory_msgs::JointTrajectoryPoint trajectory_point_c;
+    //Vectorq6x1 qvec; 
     //Vectorq6x1 home_state;
     
     new_trajectory.points.clear();
@@ -184,56 +186,40 @@ void stuff_trajectory_epsilon( std::vector<Vectorq6x1> all_qvec, trajectory_msgs
     new_trajectory.joint_names.push_back("joint_5");
     new_trajectory.joint_names.push_back("joint_6");   
 
-    new_trajectory.header.stamp = ros::Time::now();  
+    new_trajectory.header.stamp = ros::Time::now(); 
     
     
-    
-//    home_state[0] = 0.0245981;
-//    home_state[1] = -1.43767;
-//    home_state[2] = 0.0514272;
-//    home_state[3] = -0.13549;
-//    home_state[4] = -0.363732;
-//    home_state[5] = -0.0716817;
-    
-    //home state for ABBY in gazebo
-    //home_state[0] = -.226703;
-    //home_state[1] = 0.45006;
-    //home_state[2] = -1.62978;
-    //home_state[3] = 0.259641;
-    //home_state[4] = 7.44516;
-    //home_state[5] = 0.819829;
-    //Home position for ABBY (not gazebo)
-    // 0.0245981   -1.43767  0.0514272   -0.13549  -0.363732 -0.0716817
-    
-    
-    trajectory_point1.positions.clear();    
-    trajectory_point2.positions.clear(); 
+    trajectory_point_c.positions.clear();    
+    trajectory_point_t.positions.clear(); 
+    trajectory_point_h.positions.clear();
+    trajectory_point_g.positions.clear();
     //fill in the points of the trajectory: initially, all home angles
     for (int ijnt=0;ijnt<6;ijnt++) {
-        trajectory_point1.positions.push_back(g_q_state[ijnt]); // stuff in position commands for 6 joints
+        trajectory_point_c.positions.push_back(g_q_state[ijnt]); // stuff in position commands for 6 joints
         //should also fill in trajectory_point.time_from_start
-        trajectory_point2.positions.push_back(home_state[ijnt]); // stuff in position commands for 6 joints   
+        trajectory_point_t.positions.push_back(home_state[ijnt]); // stuff in position commands for 6 joints   
         //trajectory_point2.positions[ijnt] = qvec[ijnt]; //put in final position command
     }
-    trajectory_point1.time_from_start =    ros::Duration(0);  
-    trajectory_point2.time_from_start =    ros::Duration(2.0);      
+    trajectory_point_c.time_from_start =    ros::Duration(0);  
+    trajectory_point_t.time_from_start =    ros::Duration(2.0);      
 
     // start from home pose... really, should should start from current pose!
-    new_trajectory.points.push_back(trajectory_point1); // add this single trajectory point to the trajectory vector   
-    new_trajectory.points.push_back(trajectory_point2); // quick hack--return to home pose
+    new_trajectory.points.push_back(trajectory_point_c); // add this single trajectory point to the trajectory vector   
+    new_trajectory.points.push_back(trajectory_point_t); // quick hack--return to home pose
     
     // fill in the target pose: really should fill in a sequence of poses leading to this goal
-   trajectory_point2.time_from_start =    ros::Duration(4.0); 
-   for (int i = 0; i < nsolns; i++) {
-       qvec = all_qvec[i];
-       ROS_WARN("nsoln = %d", i);
+   trajectory_point_g.time_from_start =    ros::Duration(4.0); 
+   //for (int i = 0; i < nsolns; i++) {
+       //qvec = all_qvec[i];
+       //ROS_WARN("nsoln = %d", i);
          for (int ijnt=0;ijnt<6;ijnt++) {
-                trajectory_point2.positions[ijnt] = qvec[ijnt];
+                trajectory_point_g.positions[ijnt] = qvec[ijnt];
                 ROS_WARN("qvec[%d] = %f", ijnt, qvec[ijnt]);
-         } 
+         //} 
    }
 
-    new_trajectory.points.push_back(trajectory_point2); // append this point to trajectory
+    new_trajectory.points.push_back(trajectory_point_g); // append this point to trajectory
+    new_trajectory.points.push_back(trajectory_point_t);
 }
 
 
@@ -244,7 +230,7 @@ int checkSolns(std::vector<Vectorq6x1> all_qvec, int nsolns) {
        double diff = 0;
        Vectorq6x1 qvec = all_qvec[i];
        for(int j = 0; j < 6; j++) {
-           diff = diff + home_state[j] - qvec[j];
+           diff = diff + trans_state[j] - qvec[j];
        }
        all_diff[i] = diff;
        ROS_WARN("all_diff[%d] = %f", i, all_diff[i]);
@@ -279,6 +265,7 @@ int main(int argc, char** argv) {
     Irb120_fwd_solver irb120_fwd_solver; //instantiate forward and IK solvers
     Irb120_IK_solver ik_solver;
     Eigen::Vector3d n_urdf_wrt_DH,t_urdf_wrt_DH,b_urdf_wrt_DH;
+    bool begin = true;
     //Home For Abby Sim    
     //home_state[0] = 0.0245981;
     //home_state[1] = -1.43767;
@@ -289,14 +276,20 @@ int main(int argc, char** argv) {
 	
 
 // Left arm home position: [-1.5400700569152832, -1.8655591011047363, -0.4005431830883026, -0.04174210503697395, -0.8261124491691589, 1.5975639820098877]
-
-    //Home POse for Physical Abby, the actual robot...
-    home_state[0] = 5.248869570095849e-07;
-    home_state[1] = -0.8018337488174438;
-    home_state[2] = -1.08710777759552;
-    home_state[3] = -5.774702458438696e-07;
-    home_state[4] = 0.31814315915107727;
-    home_state[5] = -1.5014468601748376e-07;
+    home_state[0] = -1.5400700569152832;
+    home_state[1] = -1.8655591011047363;
+    home_state[2] = -0.4005431830883026;
+    home_state[3] = -0.04174210503697395;
+    home_state[4] = -0.8261124491691589;
+    home_state[5] = 1.5975639820098877;
+    
+    //Transition Pose for Physical Abby, the actual robot...
+    trans_state[0] = 5.248869570095849e-07;
+    trans_state[1] = -0.8018337488174438;
+    trans_state[2] = -1.08710777759552;
+    trans_state[3] = -5.774702458438696e-07;
+    trans_state[4] = 0.31814315915107727;
+    trans_state[5] = -1.5014468601748376e-07;
     // in home pose, R_urdf = I
     //DH-defined tool-flange axes point as:
     // z = 1,0,0
@@ -374,9 +367,40 @@ int main(int argc, char** argv) {
                     //ROS_WARN("qvec[%d] = %f", ijnt, qvec[ijnt]);
                     qvec = q6dof_solns[soln]; // arbitrarily choose first soln                    
                     // stuff_trajectory(qvec,new_trajectory); 
-                    stuff_trajectory_epsilon(q6dof_solns, new_trajectory, nsolns);
+                    stuff_trajectory_epsilon(qvec, new_trajectory, nsolns);
                         pub.publish(new_trajectory);
                 }
+            }
+            if (begin) {
+                begin=false;
+                trajectory_msgs::JointTrajectoryPoint trajectory_point_h;
+                trajectory_msgs::JointTrajectoryPoint trajectory_point_c;
+                
+                new_trajectory.points.clear();
+                new_trajectory.joint_names.clear();
+                new_trajectory.joint_names.push_back("joint_1");
+                new_trajectory.joint_names.push_back("joint_2");
+                new_trajectory.joint_names.push_back("joint_3");
+                new_trajectory.joint_names.push_back("joint_4");
+                new_trajectory.joint_names.push_back("joint_5");
+                new_trajectory.joint_names.push_back("joint_6");   
+                new_trajectory.header.stamp = ros::Time::now();
+                
+                trajectory_point_c.positions.clear();
+                trajectory_point_h.positions.clear();
+                for (int ijnt=0;ijnt<6;ijnt++) {
+                    trajectory_point_c.positions.push_back(g_q_state[ijnt]); // stuff in position commands for 6 joints
+                    //should also fill in trajectory_point.time_from_start
+                    trajectory_point_h.positions.push_back(home_state[ijnt]); // stuff in position commands for 6 joints   
+                    //trajectory_point2.positions[ijnt] = qvec[ijnt]; //put in final position command
+                }
+                trajectory_point_c.time_from_start =    ros::Duration(0);  
+                trajectory_point_h.time_from_start =    ros::Duration(2.0);      
+
+                // start from home pose... really, should should start from current pose!
+                new_trajectory.points.push_back(trajectory_point_c); // add this single trajectory point to the trajectory vector   
+                new_trajectory.points.push_back(trajectory_point_h); // quick hack--return to home pose
+                pub.publish(new_trajectory);
             }
             sleep_timer.sleep();    
             
